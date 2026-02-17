0691a5f72e092cec3311e1e224b7801a8212a0ac
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-02-17 17:35:09 +0700

    chore: add pm2 config as an alternative to Docker

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	apps: [
+		{
+			name: 'britzone-bot',
+			script: './dist/index.js',
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			interpreter: 'bun',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
+};
