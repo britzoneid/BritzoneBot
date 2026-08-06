0fbefd6409f10f7a4b405d2829507a8f2be75c88
Invictus Navarchus <github.com.everglade984@passmail.com>
2026-08-06 10:27:30 +0700

    refactor(permission): extract role_id to .env to make it configurable instead of hardcoded

--- src/lib/discord/permission.ts
+++ src/lib/discord/permission.ts
@@ -1,7 +1,7 @@
 import type { GuildMember } from 'discord.js';
 
 export function isBotManager(member: GuildMember): boolean {
-	const BOT_MANAGER_ID = '1534717213456597122';
+	const { ROLE_ID } = process.env;
 	const roles = member.roles.cache;
-	return roles.some((role) => role.id === BOT_MANAGER_ID);
+	return roles.some((role) => role.id === ROLE_ID);
 }
